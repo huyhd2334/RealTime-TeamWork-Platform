f22f4ffda74ee8469e00a5a7e350fb30729cee4d
huyhd2334 <huyqame1356@gmail.com>
2025-11-28 15:58:11 +0700

    change id io socket backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true
   }
